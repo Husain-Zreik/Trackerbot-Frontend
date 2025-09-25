231b6e3a52858ab825c2c769378787a722fbc979
Husain-Zreik <husain.zreik1@gmail.com>
2025-09-25 21:46:15 +0300

    edit the connect btn

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,9 @@
 const Navbar = () => {
 
+    const handleConnectToX = () => {
+        window.open('https://twitter.com/intent/follow?screen_name=trackerbot_fun', '_blank');
+    };
+
     return (
         <nav className="navbar">
             <div className="navbar__block"></div>
@@ -7,7 +11,15 @@ const Navbar = () => {
                 <div className="navbar__content">
                     <div className="navbar__brand">trackerbot.fun</div>
                     <div className="navbar__nav">
-                        <a href="#" className="navbar__nav-link">Leaderboard</a>
+                        <button
+                            onClick={handleConnectToX}
+                            className="navbar__connect-btn"
+                        >
+                            <svg className="navbar__connect-icon" viewBox="0 0 24 24" fill="currentColor">
+                                <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+                            </svg>
+                            Connect
+                        </button>
                     </div>
                 </div>
             </div>
